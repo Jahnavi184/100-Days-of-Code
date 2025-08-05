f90cda83e2b5cfff31fc419f210af532ec081d2c
Jahnavi184 <jahnavisuresh21@gmail.com>
2025-08-05 20:26:05 +0530

    Create Day-1 -Band Name Generator.py
    This program generates a band name by combining the city name and the pet name that you provide.

---
+++ Day-1 -Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+cityName=input("Enter the name of the city you grew up in\n")
+petName=input("Enter the name of your pet\n")
+print("Your Band name could be:" + cityName + " " + petName)
